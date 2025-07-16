3872349faab088147d1d3aeda1e13bae581a2aeb
Titas <titaslr38@gmail.com>
2025-07-16 13:17:20 +0300

    Added CPU and Memory display

--- SystemMonitor/Form1.Designer.cs
+++ SystemMonitor/Form1.Designer.cs
@@ -29,12 +29,91 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.labelCpu = new System.Windows.Forms.Label();
+            this.progressBarCpu = new System.Windows.Forms.ProgressBar();
+            this.labelCpuValue = new System.Windows.Forms.Label();
+            this.labelMemoryValue = new System.Windows.Forms.Label();
+            this.labelMemory = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // labelCpu
+            // 
+            this.labelCpu.AutoSize = true;
+            this.labelCpu.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(186)));
+            this.labelCpu.Location = new System.Drawing.Point(12, 9);
+            this.labelCpu.Name = "labelCpu";
+            this.labelCpu.Size = new System.Drawing.Size(119, 25);
+            this.labelCpu.TabIndex = 0;
+            this.labelCpu.Text = "CPU usage:";
+            // 
+            // progressBarCpu
+            // 
+            this.progressBarCpu.Location = new System.Drawing.Point(196, 9);
+            this.progressBarCpu.Name = "progressBarCpu";
+            this.progressBarCpu.Size = new System.Drawing.Size(483, 23);
+            this.progressBarCpu.TabIndex = 1;
+            // 
+            // labelCpuValue
+            // 
+            this.labelCpuValue.AutoSize = true;
+            this.labelCpuValue.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(186)));
+            this.labelCpuValue.Location = new System.Drawing.Point(685, 7);
+            this.labelCpuValue.Name = "labelCpuValue";
+            this.labelCpuValue.Size = new System.Drawing.Size(41, 25);
+            this.labelCpuValue.TabIndex = 2;
+            this.labelCpuValue.Text = "0%";
+            this.labelCpuValue.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // labelMemoryValue
+            // 
+            this.labelMemoryValue.AutoSize = true;
+            this.labelMemoryValue.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(186)));
+            this.labelMemoryValue.Location = new System.Drawing.Point(191, 38);
+            this.labelMemoryValue.Name = "labelMemoryValue";
+            this.labelMemoryValue.Size = new System.Drawing.Size(58, 25);
+            this.labelMemoryValue.TabIndex = 5;
+            this.labelMemoryValue.Text = "0 MB";
+            // 
+            // labelMemory
+            // 
+            this.labelMemory.AutoSize = true;
+            this.labelMemory.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(186)));
+            this.labelMemory.Location = new System.Drawing.Point(12, 38);
+            this.labelMemory.Name = "labelMemory";
+            this.labelMemory.Size = new System.Drawing.Size(173, 25);
+            this.labelMemory.TabIndex = 3;
+            this.labelMemory.Text = "Available memory:";
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.labelMemoryValue);
+            this.Controls.Add(this.labelMemory);
+            this.Controls.Add(this.labelCpuValue);
+            this.Controls.Add(this.progressBarCpu);
+            this.Controls.Add(this.labelCpu);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label labelCpu;
+        private System.Windows.Forms.ProgressBar progressBarCpu;
+        private System.Windows.Forms.Label labelCpuValue;
+        private System.Windows.Forms.Label labelMemoryValue;
+        private System.Windows.Forms.Label labelMemory;
+        private System.Windows.Forms.Timer timer1;
     }
 }
 
--- SystemMonitor/Form1.cs
+++ SystemMonitor/Form1.cs
@@ -7,14 +7,45 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Diagnostics;
 
 namespace SystemMonitor
 {
     public partial class Form1 : Form
     {
+        private PerformanceCounter cpuCounter;
+        private PerformanceCounter memoryCounter;
+
         public Form1()
         {
             InitializeComponent();
+
+            cpuCounter = new PerformanceCounter("Processor", "% Processor Time", "_Total");
+            memoryCounter = new PerformanceCounter("Memory", "Available MBytes");
+
+            timer1.Tick += Timer1_Tick;
+            timer1.Start();
+        }
+
+
+        private void Timer1_Tick(object sender, EventArgs e)
+        {
+            // Get values
+            float cpuUsage = cpuCounter.NextValue();
+            float availableMemory = memoryCounter.NextValue();
+
+            // Update labels
+            labelCpuValue.Text = $"{cpuUsage:F1}%";
+            labelMemoryValue.Text = $"{availableMemory} MB";
+
+            // Update progress bar (clip value to 100)
+            int cpuInt = (int)Math.Min(cpuUsage, 100);
+            progressBarCpu.Value = cpuInt;
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
